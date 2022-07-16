8de4aad088dab349a00d345987f208b8bc62a055
daniel8757 <69530004+daniel20087@users.noreply.github.com>
2022-07-16 16:56:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("how many obama's: ");
+            int obama = Convert.ToInt32(Console.ReadLine());
+            Console.Write("how many cares: ");
+            int care = Convert.ToInt32(Console.ReadLine());
+            Console.Write("what last name character: ");
+            string? lastname = Console.ReadLine();
+            for (int i = 0; i < obama; i++)
+            {
+                for (int j = 0; j < care; j++)
+                {
+                    Console.Write(lastname);
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
